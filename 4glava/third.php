c403dbb7e1087cea4faa552749f41b5bd89774f6
Sararun <sarosekvlad12@gmail.com>
2022-07-19 16:59:41 +0700

    Update README.md

---
+++ 4glava/third.php
@@ -0,0 +1,7 @@
+<?php
+include "second.php";
+$image_path='/image';
+print html_url('Hhsx44tJz3s.jpg');
+print html_url('vnop5lmSb-k.jpg', 'fuzzy');
+print html_url('StKSjGFlA58.jpg',null,640,480);
+//В этом упражнение надо воспользоваться функцией созданной в другой программе
