c25c785b90848eb9436afdff3bb66a7c7f14c39a
Sararun <sarosekvlad12@gmail.com>
2022-07-25 03:59:59 +0700

    Update README.md

--- 6glava/FormHelper.php
+++ 6glava/FormHelper.php
@@ -1,81 +1,97 @@
 <?php
-class FormHelper{
-    protected $values=array();
-
-    public function __construct($values=array()){
-        if($_SERVER['REQUEST_METHOD'] == 'POST'){
-            $this->values=$values;
-
-        }else{
-            $this->$values=$_POST;
+class FormHelper {
+    protected $values = array();
+    public function __construct($values = array()) {
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $this->values = $_POST;
+        } else {
+            $this->values = $values;
         }
     }
 
-    public function input($type, $attributes=array(), $isMultiple=false){
-        $attributes['type']=$type;
-        if(($type=='radio') || ($type=='checkbox')){
-            if($this->isOptionSelected($attributes['name'] ?? null, $attributes['value'] ?? null)){
-                $attributes['checked']=true;
+    public function input($type, $attributes = array(),
+                          $isMultiple = false) {
+        $attributes['type'] = $type;
+        if (($type == 'radio') || ($type == 'checkbox')) {
+            if ($this->isOptionSelected($attributes['name']
+                ?? null, $attributes['value'] ?? null)) {
+                $attributes['checked'] = true;
             }
         }
         return $this->tag('input', $attributes, $isMultiple);
     }
 
-    public function select($options, $attributes=array()){
-        $multiple=$attributes['multiple'] ?? false;
+    public function select($options, $attributes = array()) {
+        $multiple = $attributes['multiple'] ?? false;
         return
-            $this->start('select', $attributes, $multiple).
-            $this->options($attributes['name'] ?? null, $options).
+            $this->start('select', $attributes, $multiple) .
+            $this->options($attributes['name'] ?? null, $options) .
             $this->end('select');
     }
 
-    public function textarea($attribtes=array()){
-        $name=$attribtes['name'] ?? null;
-        $value=$this->values[$name]??'';
-        return $this->start('textarea', $attribtes).
-            htmlentities($value).
+    public function textarea($attributes = array()) {
+        $name = $attributes['name'] ?? null;
+        $value = $this->values[$name] ?? '';
+        return $this->start('textarea', $attributes) .
+            htmlentities($value) .
             $this->end('textarea');
     }
 
-    public function tag($tag,$attributes = array(), $isMultiple=false){
+    public function tag($tag, $attributes = array(),
+                        $isMultiple = false) {
         return
-        "<$tag {$this->attributes($attributes, $isMultiple)}/>";
+            "<$tag {$this->attributes($attributes, $isMultiple)} />";
     }
 
-    public function start($tag, $attributes=array(), $isMultiple=false){
-        $valueAttribute = (! (($tag == 'select') || ($tag == 'textarea')));
-        $attrs = $this->attributes($attributes, $isMultiple, $valueAttribute);
+    public function start($tag, $attributes = array(),
+                          $isMultiple = false) {
+// Дескрипторы <select> и <textarea> не получают
+// атрибуты value
+        $valueAttribute =
+            (! (($tag == 'select')||($tag == 'textarea')));
+        $attrs = $this->attributes($attributes, $isMultiple,
+            $valueAttribute);
         return "<$tag $attrs>";
     }
-
-    public function  end($tag){
+    public function end($tag) {
         return "</$tag>";
     }
+    protected function attributes($attributes, $isMultiple, $valueAttribute = true) {
+        $tmp = array();
 
-    protected function attributes($attributes, $isMultiple, $valueAttribute = true ){
-        $tmp=array();
-        if($valueAttribute && isset($attributes['name']) && array_key_exists($attributes['name'], $this->values)){
-            $attributes['value']=$this->values[$attributes['name']];
+        // Если данный дескриптор может содержать атрибут value,
+        // а его имени соответствует элемент в массиве значений,
+        // то установить этот атрибут
+        if ($valueAttribute && isset($attributes['name'])
+            && array_key_exists($attributes['name'],
+                $this->values)) {
+            $attributes['value'] =
+                $this->values[$attributes['name']];
         }
-        foreach($attributes as $k=>$v){
-            if(is_bool($v)){
-                if($v) {$tmp[] = $this->encode($k);}
+        foreach ($attributes as $k => $v) {
+        // Истинное логическое значение означает
+        // логический атрибут
+            if (is_bool($v)) {
+                if ($v) { $tmp[] = $this->encode($k); }
             }
-            else{
-                $value=$this->encode($v);
-                if($isMultiple && ($k == 'name')){
+        // иначе k = v
+            else {
+                $value = $this->encode($v);
+        // Если это многозначный элемент, присоединить
+        // квадратные скобки ([]) к его имени
+                if ($isMultiple && ($k == 'name')) {
                     $value .= '[]';
                 }
-                $tmp[]="$k=\"$value\"";
+                $tmp[] = "$k=\"$value\"";
             }
         }
         return implode(' ', $tmp);
-    }
+        }
 
-    protected function options($name, $options){
-        $tmp=array();
+    protected function options($name, $options) {
+        $tmp = array();
         foreach($options as $k => $v){
-            $s = "<option value=\"{this->encode($k)}\"";
+            $s = "<option value=\"{$this->encode($k)}\"";
             if($this->isOptionSelected($name, $k)){
                 $s .=' selected';
             }
--- 6glava/math-form.php
+++ 6glava/math-form.php
@@ -1,32 +1,29 @@
-<form method="post" action=
-"<?=$form->encode($_SERVER['PHP_SELF']) ?>">
+<form method="POST" action=
+"<?= $form->encode($_SERVER['PHP_SELF']) ?>">
     <table>
-        <?php if(isset($errors)) { ?>
+        <?php if ($errors) { ?>
         <tr>
-            <td>Not correct</td>
+            <td>You need to correct the following errors:</td>
             <td><ul>
-                    <?php foreach ($errors as $error){?>
-                    <li><?=$form->encode($error)?></li>
-                    <?php }?>
-                    </ul></td>
-            <?php }?>
-
-        <tr><td>First</td>
-            <td><?=$form->input('text', ['name'=> 'num1'])?></td>
+                    <?php foreach ($errors as $error) { ?>
+                        <li><?= $form->encode($error) ?></li>
+                    <?php } ?>
+                </ul></td>
+            <?php } ?>
+        <tr><td>First Number :</td>
+            <td><?= $form->input('text', ['name' => 'num1']) ?></td>
         </tr>
-        <tr><td>Oper:</td>
-            <td><?=$form->select($GLOBALS['aper'], ['name'=>'ap'])?></td>
+        <tr><td>Operation: </td>
+            <td><?= $form->select($GLOBALS['aper'], ['name' => 'ap']) ?></td>
         </tr>
-
-        <tr><td>Second</td>
-            <td><?=$form->input('text', ['name'=> 'num2'])?></td>
+        <tr><td>Second Number:</td>
+            <td><?= $form->input('text', ['name' => 'num2']) ?></td>
         </tr>
         <tr><td colspan="2" align="center">
-                <?=$form->input('submit', ['value'=>'Calculate'])?>
+                <?= $form->input('submit', ['value' => 'Calculate']) ?>
             </td></tr>
     </table>
 </form>
-
 
 
 
