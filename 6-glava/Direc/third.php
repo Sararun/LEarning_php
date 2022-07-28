8c399d20673b8f9a79b7b09554015f1e8c802e48
Sararun <sarosekvlad12@gmail.com>
2022-07-29 00:08:15 +0700

    Update README.md

--- 6-glava/Direc/third.php
+++ 6-glava/Direc/third.php
@@ -8,81 +8,97 @@ $states = [ 'AL', 'АК', 'AZ', 'AR', 'СА', 'СО', 'СТ', 'DC',
     'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA',
     'WV', 'WI', 'WY' ];
 
-//Проверка обработка формы, это делается один раз, поэтому функция не нужна
-if($_SERVER['REQUEST_METHOD'] == 'POST'){
-    list($errors, $input)=validateForm();
-    if($errors){
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    list($errors, $input) = validateForm();
+    if ($errors) {
         showForm($errors);
-    }else{
-        processForm($errors);
+    } else {
+        processForm($input);
     }
-}else{
+} else {
     showForm();
 }
-function showForm($errors=array()){    //Будет передавать данные в html
-    $form=new FormHelper();
+function showForm($errors = array()) {
+    $form = new FormHelper();
     include 'shipping-form.php';
 }
-function validateForm(){   //Правильность вводимой информации
+function validateForm() {
     $input = array();
     $errors = array();
-    foreach (['from', 'to'] as $addr) {
-        foreach (['Name' => 'name', 'Addres 1' => 'address1',
-                     'City' => 'city', 'State' => 'state'] as $label => $field) {
-            $input[$addr . '_' . $field] = $_POST[$addr . '_' . $field] ?? '';
-            if (strlen($input[$addr . '_' . $field]) == 0) {
-                $errors[] = "Please enter value";
+    foreach (['from','to'] as $addr) {
+// проверить обязательные поля
+        foreach(['Name' => 'name', 'Address 1' => 'address1', 'City' => 'city', 'State' => 'state']
+                as $label => $field) {
+            $input[$addr.'_'.$field] = $_POST[$addr.'_'.$field] ?? '';
+            if (strlen($input[$addr.'_'.$field]) == 0) {
+                $errors[] = "Please enter a value for $addr $label.";
             }
         }
-        $input[$addr . '_state'] = $GLOBALS['state'][$input[$addr . '_state']] ?? '';
-        if (!in_array($input[$addr . '_state'], $GLOBALS['states'])) {
-            $errors[] = "Please select correct value";
+// проверить штат
+        $input[$addr.'_state'] =$GLOBALS['states'][$input[$addr.'_state']] ?? '';
+        if (! in_array($input[$addr.'_state'], $GLOBALS['states'])) {
+            $errors[] = "Please select a valid $addr state.";
         }
-        $input[$addr . '_zip'] = filter_input(INPUT_POST, $addr . '_zip',
-            FILTER_VALIDATE_INT, ['options' => ['min_range' => 10000, 'max_range' => 99999]]);
-
-        if (is_null($input[$addr . '_zip']) || ($input[$addr . '_zip'] === false)) {
+// проверить почтовый индекс
+        $input[$addr.'_zip'] = filter_input(INPUT_POST, $addr.'_zip',
+            FILTER_VALIDATE_INT, ['options' =>
+                ['min_range'=>10000,
+                    'max_range'=>99999]]);
+        if (is_null($input[$addr.'_zip']) ||
+            ($input[$addr.'_zip']===false)) {
             $errors[] = "Please enter a valid $addr ZIP";
         }
-        $input[$addr . '_address2'] = $_POST[$addr . '_address2'] ?? '';
+// He забыть о втором адресе address2!
+        $input[$addr.'_address2'] = $_POST[$addr.'_address2'] ?? '';
     }
-    foreach (['height', 'width', 'depth', 'weight'] as $field) {
-        $input[$field] = filter_input(INPUT_POST, $field,
+/* высота, ширина, глубина, вес посылки должны быть
+ выражены числовыми положительными значениями*/
+    foreach(['height','width','depth','weight'] as $field) {
+        $input[$field] =filter_input(INPUT_POST, $field,
             FILTER_VALIDATE_FLOAT);
-        if (!($input[$field] && ($input[$field] > 0))) {
-            $errors[] = "Please enter a valid $field";
+/* Нулевое значение является недействительным, поэтому
+ проверить следующее условие только на истинность вместо
+ того, чтобы проверять на пустое или ложное значение*/
+        if (! ($input[$field] && ($input[$field] > 0))) {
+            $errors[] = "Please enter a valid $field.";
         }
     }
+// проверить вес посылки
     if ($input['weight'] > 150) {
-        $errors[] = "The package must weight no more 150";
+        $errors[] = "The package must weigh no more than 150 lbs.";
     }
-    foreach (['height', 'width', 'depth'] as $dim) {
+// проверить размеры посылки
+    foreach(['height', 'width','depth'] as $dim) {
         if ($input[$dim] > 36) {
-            $errors[] = "The package $dim must be no more than 36";
+            $errors[] = "The package $dim must be no more
+            than 36 inches.";
         }
     }
-    return array($errors, $input);
+return array($errors, $input);
 }
-function processForm($input){
+function processForm($input) {
+// создать шаблон для отчета
     $tpl=<<<HTML
     <p>Your package is {height}" x {width}" x {depth}" and weights {weight}</p>
     <p>IT is coming from:</p>
     <pre>
-    {form_name}
+    {from_name}
     {from_address}
     {from_city}, {from_state}, {from_zip} 
     </pre>
     <p>It is going to</p>
     <pre>
     {to_name}
-    {toaddress}
+    {to_address}
     {to_city}, {to_state}, {to_zip}
     </pre>
     HTML;
-    foreach(['form','to'] as $addr){
+    foreach(['from','to'] as $addr){
         $input[$addr.'_address']=$input[$addr.'_address1'];
         if(strlen($input[$addr.'_address2'])){
-            $input[$addr.'_address'] .= "\n" . $input[$addr.'address2'];
+            $input[$addr.'_address'] .= "\n"
+                . $input[$addr.'_address2'];
         }
     }
     $html = $tpl;
